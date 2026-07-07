d364f22dc9e9ef1d85ae97f188999d043cb0b6df
iamntoko <ntoko.ndie@iamntoko.com>
2026-07-07 00:11:17 -0500

    feat: fetch and render character list from swapi

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const subHeading = document.getElementById('sub-heading');
-subHeading.textContent = 'Explore Star Wars Characters and Films';
+async function fetchPeople() {
+    try {
+        const response = await fetch('https://www.swapi.tech/api/people');
+        
+        if (!response.ok) {
+
+            throw new Error('Failed to fetch people');
+        }
+
+        const data = await response.json();
+        
+        console.log(data.results);
+
+        const characterList = document.getElementById('character-list');
+
+        data.results.forEach(person => {
+            const li = document.createElement('li');
+            li.textContent = person.name;
+            characterList.appendChild(li);
+        });
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+fetchPeople();
